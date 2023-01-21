860422baf42a57036db9f80409ec48e6fbdec759
Bilol <mamatqulov@gmail.com>
2023-01-21 16:34:23 +0500

    find_number_of_messages

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -1,6 +1,7 @@
 from read_data import read_data
 
-def find_number_of_messages(data: dict)->int:
+
+def find_number_of_messages(data: dict) -> int:
     """
     Get the total number of messages.
 
@@ -8,6 +9,14 @@ def find_number_of_messages(data: dict)->int:
         data (dict): Dictionary containing the data of the json file.
     Returns:
         int: Total number of messages.
-    
+
     """
-    return
+    num_messag = 0
+    for n in range(len(data['messages'])):
+        if data['messages'][n]['text']:
+            num_messag += 1
+    return num_messag
+
+
+data = read_data('data/result.json')
+print(find_number_of_messages(data))
