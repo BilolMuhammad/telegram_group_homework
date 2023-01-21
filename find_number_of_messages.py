f5930bec6a188769ecfc7b5b08567e14fcb20146
Bilol <mamatqulov@gmail.com>
2023-01-21 17:21:13 +0500

    find_number_of_messages

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -13,7 +13,7 @@ def find_number_of_messages(data: dict) -> int:
     """
     num_messag = 0
     for n in range(len(data['messages'])):
-        if data['messages'][n]['text']:
+        if 'text' in data['messages'][n]:
             num_messag += 1
     return num_messag
 
